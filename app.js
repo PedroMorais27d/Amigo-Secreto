b0d2a3dc384b833cddd04456296295f3d6b82e93
PedroMorais27d <8bitss2@gmail.com>
2025-02-06 18:28:00 -0300

    segundo commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = [];
+let nome;
+
+
+txtPadrao();
+
+function exibirTxt(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function adicionarAmigo() {
+    nome = document.querySelector('#amigo').value;
+    if (nome) {
+        nomes.push(nome);
+        limparCampo();
+        exibirTxt('p', 'Nome adicionado: ' + nome);
+        atualizarListaAmigos();
+    } else {
+       alert('Por favor, digite um nome.');
+    }
+}
+
+function atualizarListaAmigos(){
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML="";
+
+    for (let i = 0; i < nomes.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = nomes[i]; 
+        lista.appendChild(item); 
+    }
+}
+
+function sortearAmigo(){
+
+    if(nomes.length==0){
+        document.querySelector('#resultado').innerHTML = "Nenhum nome disponível para o sorteio.";
+        return;
+    }
+
+    let numRand =  Math.floor(Math.random() * nomes.length);
+    let sorteado = nomes[numRand];
+
+    document.querySelector('#resultado').innerHTML = "Você sorteou: "+sorteado;
+
+    nomes = [];
+    atualizarListaAmigos();
+
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value='';
+}
